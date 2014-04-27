ad4029a3d147f55b06f778ea3552486190d3cbdc
hduser <hduser@florix.(none)>
2014-04-27 18:56:54 +0200

    Initial commit

---
+++ src/main/java/com/wireframe/App.java
@@ -0,0 +1,13 @@
+package com.wireframe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
